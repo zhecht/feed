2ba44cf5e7d2edea10fbcf19b7a4bcea2af484d3
zackhecht <zackhecht@Zacks-MacBook-Air.local>
2025-04-12 19:31:33 -0400

    test

--- feed.py
+++ feed.py
@@ -13,6 +13,11 @@ from bs4 import BeautifulSoup as BS
 from shared import *
 from datetime import datetime, timedelta
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 def commitChanges():
 	repo = git.Repo(".")
 	repo.git.add(A=True)
@@ -21,84 +26,76 @@ def commitChanges():
 	origin = repo.remote(name="origin")
 	origin.push()
 
-async def writeFeed(date, loop):
+def writeFeed(date, loop):
 	if not date:
 		date = str(datetime.now())[:10]
 	url = f"https://baseballsavant.mlb.com/gamefeed?date={date}&hf=exitVelocity"
-	browser = await uc.start(no_sandbox=True)
-	page = await browser.get(url)
-	await page.wait_for(selector=".container-open")
-	time.sleep(5)
-
-	#b = await page.query_selector("#btnHide")
-	#await b.click()
-
-	# click exit velo table
-	if False:
-		o = await page.query_selector(".container-open")
-		id = o.get("id").split("-")[-1]
-		#print(id)
-		await o.click()
-		btn = await page.query_selector(f"#button_exitVelocity_{id}")
-		await btn.click()
+	driver = webdriver.Firefox()
+	driver.get(f"https://baseballsavant.mlb.com/gamefeed?hf=exitVelocity&date={date}")
+	try:
+		element = WebDriverWait(driver, 10).until(
+			EC.presence_of_element_located((By.CLASS_NAME, "container-open"))
+		)
+	except:
+		driver.quit()
 
 	with open("feed_times.json") as fh:
 		times = json.load(fh)
 
-	schedule_url = "https://raw.githubusercontent.com/zhecht/playerprops/main/static/mlb/schedule.json"
-	response = requests.get(schedule_url)
+	headers = {"Accept": "application/vnd.github.v3.raw"}
+	url = "https://api.github.com/repos/zhecht/playerprops/contents/static/mlb/schedule.json"
+	response = requests.get(url, headers=headers)
 	schedule = response.json()
 
 	i = 0
 	while True:
-		html = await page.get_content()
-		with open(f"feed.html", "w") as fh:
-			fh.write(html)
+		html = driver.page_source
+		soup = BS(html, "html.parser")
+		#with open(f"feed.html", "w") as fh:
+		#	fh.write(html)
 
 		games = []
 		for gameData in schedule[date]:
 			if gameData["start"] and gameData["start"] != "LIVE":
-				dt = datetime.strptime(gameData["start"], "%I:%M %p")
-				dt = int(dt.strftime("%H%M"))
-				#print(dt, int(datetime.now().strftime("%H%M")))
+				try:
+					dt = datetime.strptime(gameData["start"], "%I:%M %p")
+					dt = int(dt.strftime("%H%M"))
+				except:
+					continue
+
 				if dt <= int(datetime.now().strftime("%H%M")):
 					games.append(gameData)
 		data = {}
-		parseFeed(data, times, games, len(schedule[date]), loop)
+		parseFeed(data, times, games, len(schedule[date]), soup)
 		i += 1
 
 		if not loop:
 			break
-		
+
 		time.sleep(1)
 		if i >= 5:
 			commitChanges()
 			i = 0
 
-	browser.stop()
+	driver.quit()
 
-def parseFeed(data, times, games, totGames, loop):
-	soup = BS(open("feed.html", 'rb').read(), "lxml")
+def parseFeed(data, times, games, totGames, soup):
 	allTable = soup.find("div", id="allMetrics")
 	hdrs = [th.text.lower() for th in allTable.find_all("th")]
 	starts = {}
 	for game in games:
 		starts[game["game"]] = game["start"]
+	data = {}
 	data["all"] = {k: v.text.strip() for k,v in zip(hdrs,allTable.find_all("td")) if k}
 	data["all"]["liveGames"] = len(games)
 	data["all"]["totGames"] = totGames
-	data["all"]["updated"] = str(datetime.now())
 	for div in soup.find_all("div", class_="game-container"):
 		away = div.find("div", class_="team-left")
 		home = div.find("div", class_="team-right")
 		away = convertMLBTeam(away.text.strip())
 		home = convertMLBTeam(home.text.strip())
 		game = f"{away} @ {home}"
-		if game in data:
-			game += "-gm2"
 		data[game] = []
-		if game not in times:
-			times[game] = {}
 		table = div.find("div", class_="exit-velocity-table")
 		if not table or not table.find("tbody"):
 			continue
@@ -111,19 +108,16 @@ def parseFeed(data, times, games, totGames, loop):
 			hrPark = tds[-1].text.strip()
 
 			pa = tds[5].text.strip()
-			seen = pa in times[game]
-			dt = times[game].get(pa, str(datetime.now()).split(".")[0])
-			times[game][pa] = dt
 			j = {
 				"player": player,
 				"pitcher": pitcher,
 				"game": game,
 				"hr/park": hrPark,
 				"pa": pa,
-				"dt": dt,
+				"dt": "",
 				"img": img,
 				"team": team,
-				"start": starts[game]
+				"start": starts.get(game, "")
 			}
 			i = 6
 			for hdr in ["in", "result", "evo", "la", "dist", "speed", "mph", "xba"]:
@@ -136,6 +130,11 @@ def parseFeed(data, times, games, totGames, loop):
 		json.dump(data, fh, indent=4)
 	with open("feed_times.json", "w") as fh:
 		json.dump(times, fh, indent=4)
+	with open("feed_times_historical.json") as fh:
+		hist = json.load(fh)
+	hist[str(datetime.now())[:10]] = times
+	with open("feed_times_historical.json", "w") as fh:
+		json.dump(hist, fh)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -166,4 +165,4 @@ if __name__ == '__main__':
 			json.dump({}, fh)
 		exit()
 
-	uc.loop().run_until_complete(writeFeed(date, args.loop))
+	writeFeed(date, args.loop)
